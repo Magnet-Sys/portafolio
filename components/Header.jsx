21ab5a16fad7e2bc1f1e8c722bd52b8617ace5bd
Magnet-Sys <80355938+Magnet-Sys@users.noreply.github.com>
2024-06-16 11:50:17 -0400

    changed name

--- components/Header.jsx
+++ components/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
         {/* logo */}
         <Link href="/">
           <h1 className="text-4xl font-semibold">
-            Dianna<span className="text-accent">.</span>
+            RYU / ROONEY / ROMÁN<span className="text-accent">.</span>
           </h1>
         </Link>
         {/* desktop nav & hire me button*/}
